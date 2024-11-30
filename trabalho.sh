bc28aa186447a9cc616aa680d4872e1ad1519bd7
mauriciopara <paramauricio7@gmail.com>
2024-11-29 23:56:01 -0500

    TrabalhoRederecionamentos

--- trabalho.sh
+++ trabalho.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+sudo apt update
+sudo apt install fortune-mod -y
+
+
+fortune > mensagem2.txt
+fortune > mensagem3.txt
+fortune > mensagem1.txt
+
+cat mensagem1.txt
+cat mensagem2.txt
+cat mensagem3.txt
+
+cat mensagem1.txt mensagem2.txt mensagem3.txt > mensagens.txt
+cat mensagens.txt
+
+cp mensagens.txt mensagens_antiga.txt
+fortune >> mensagens.txt
+
+wc -l < mensagens.txt > linhas.txt
+
+grep -o 'a' mensagens.txt | wc -l > letras.txt
+
+ls -R /etc > saida.txt 2> erros.txt
+
+ls -R /etc > saida_com_erros.txt 2>&1
+
+cut -d':' -f1 /etc/passwd > usuarios.txt
+
+cut -d':' -f1 /etc/passwd | sort > usuarios_ordenados.txt 
